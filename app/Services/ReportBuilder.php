218cb5add998427c1d42963ce63c1f381a892275
OnLeeTwo <owent.ovandy7777@gmail.com>
2025-06-24 23:37:04 +0700

    fix: server error

--- app/Services/ReportBuilder.php
+++ app/Services/ReportBuilder.php
@@ -1,39 +1,37 @@
 <?php
 
-public function build(string $table, array $columns, array $filters = [])
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+
+class ReportBuilder
 {
-    $query = DB::table($table)->select($columns);
+    public function build(string $table, array $columns, array $filters = [])
+    {
+        $query = DB::table($table)->select($columns);
 
-    foreach ($filters as $column => $value) {
-        if (is_array($value)) {
-            if (isset($value['range']) && is_array($value['range'])) {
-                [$start, $end] = $value['range'] + [null, null];
-
-                if ($start !== null) {
-                    $query->where($column, '>=', $start);
+        foreach ($filters as $column => $value) {
+            if (is_array($value)) {
+                // Handle "range"
+                if (isset($value['range']) && is_array($value['range'])) {
+                    [$start, $end] = $value['range'] + [null, null];
+                    if ($start !== null) $query->where($column, '>=', $start);
+                    if ($end !== null) $query->where($column, '<=', $end);
                 }
-                if ($end !== null) {
-                    $query->where($column, '<=', $end);
+                // Handle min/max
+                elseif (isset($value['min']) || isset($value['max'])) {
+                    if (isset($value['min'])) $query->where($column, '>=', $value['min']);
+                    if (isset($value['max'])) $query->where($column, '<=', $value['max']);
                 }
+                // whereIn fallback
+                else {
+                    $query->whereIn($column, $value);
+                }
+            } else {
+                $query->where($column, $value);
             }
-
-            // Handle "min" / "max" filter
-            elseif (isset($value['min']) || isset($value['max'])) {
-                if (isset($value['min'])) {
-                    $query->where($column, '>=', $value['min']);
-                }
-                if (isset($value['max'])) {
-                    $query->where($column, '<=', $value['max']);
-                }
-            }
-
-            else {
-                $query->whereIn($column, $value);
-            }
-        } else {
-            $query->where($column, $value);
         }
-    }
 
-    return $query->get();
+        return $query->get();
+    }
 }
