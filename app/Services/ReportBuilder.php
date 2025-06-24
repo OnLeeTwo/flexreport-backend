7636f4dfd621023427c02f7735b9d786466c0f94
OnLeeTwo <owent.ovandy7777@gmail.com>
2025-06-24 23:24:31 +0700

    fix: date and range filter

--- app/Services/ReportBuilder.php
+++ app/Services/ReportBuilder.php
@@ -1,23 +1,39 @@
 <?php
 
-namespace App\Services;
-
-use Illuminate\Support\Facades\DB;
-
-class ReportBuilder
+public function build(string $table, array $columns, array $filters = [])
 {
-    public function build(string $table, array $columns, array $filters = [])
-    {
-        $query = DB::table($table)->select($columns);
+    $query = DB::table($table)->select($columns);
 
-        foreach ($filters as $column => $value) {
-            if (is_array($value)) {
-                $query->whereIn($column, $value);
-            } else {
-                $query->where($column, $value);
+    foreach ($filters as $column => $value) {
+        if (is_array($value)) {
+            if (isset($value['range']) && is_array($value['range'])) {
+                [$start, $end] = $value['range'] + [null, null];
+
+                if ($start !== null) {
+                    $query->where($column, '>=', $start);
+                }
+                if ($end !== null) {
+                    $query->where($column, '<=', $end);
+                }
             }
-        }
 
-        return $query->get();
+            // Handle "min" / "max" filter
+            elseif (isset($value['min']) || isset($value['max'])) {
+                if (isset($value['min'])) {
+                    $query->where($column, '>=', $value['min']);
+                }
+                if (isset($value['max'])) {
+                    $query->where($column, '<=', $value['max']);
+                }
+            }
+
+            else {
+                $query->whereIn($column, $value);
+            }
+        } else {
+            $query->where($column, $value);
+        }
     }
+
+    return $query->get();
 }
